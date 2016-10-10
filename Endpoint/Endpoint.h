57893e1b2cb01a1a656cff41453861530173d8a2
Peter Weishapl <pw@tailored-apps.com>
2016-10-10 16:35:53 +0200

    Initial Commit

---
+++ Endpoint/Endpoint.h
@@ -0,0 +1,19 @@
+//
+//  Endpoint.h
+//  Endpoint
+//
+//  Created by Peter W on 10/10/2016.
+//  Copyright © 2016 Tailored Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Endpoint.
+FOUNDATION_EXPORT double EndpointVersionNumber;
+
+//! Project version string for Endpoint.
+FOUNDATION_EXPORT const unsigned char EndpointVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Endpoint/PublicHeader.h>
+
+
