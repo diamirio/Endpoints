4003af3c50a09b8a70c159c5dc9a5af4fff44b48
Peter Weishapl <pw@tailored-apps.com>
2016-10-13 21:35:43 +0200

    more cleanup

--- Endpoint/API.swift
+++ Endpoint/API.swift
@@ -16,63 +16,10 @@ public enum APIError: Error {
 
 open class API {
     public let baseURL: URL
+    public var debugAll = false
     
     public init(baseURL: URL) {
         self.baseURL = baseURL
-    }
-    
-    open func validate(response: HTTPURLResponse) -> Error? {
-        let code = response.statusCode
-        if !(200..<300).contains(code) {
-            return APIError.unacceptableStatus(code: code)
-        }
-        
-        return nil
-    }
-    
-    @discardableResult
-    public func call<E: Endpoint, R: RequestEncoder, P: ParsableResponse>(endpoint: E, with data: R?=nil, session: URLSession=URLSession.shared, debug: Bool=false, completion: @escaping (Result<P>)->()) -> URLSessionDataTask where E.RequestType == R, E.ResponseType == P {
-        let request = self.request(for: endpoint, with: data)
-        
-        return start(request: request, for: endpoint, session: session, debug: debug, completion: completion)
-    }
-    
-    @discardableResult
-    public func start<E: Endpoint, P: ParsableResponse>(request: URLRequest, for endpoint: E, session: URLSession=URLSession.shared, debug: Bool=false, completion: @escaping (Result<P>)->()) -> URLSessionDataTask where E.ResponseType == P {
-        return start(request: request, responseType: P.self, session: session, debug: debug, completion: completion)
-    }
-    
-    @discardableResult
-    public func start<P: ParsableResponse>(request: URLRequest, responseType: P.Type, session: URLSession=URLSession.shared, debug: Bool=false, completion: @escaping (Result<P>)->()) -> URLSessionDataTask {
-        let task = session.dataTask(with: request) { data, response, error in
-            if debug {
-                dump(request)
-            }
-            
-            var result = Result<P>(response: response as? HTTPURLResponse)
-            
-            if let error = error {
-                result.error = error
-            } else if let response = result.response {
-                if let error = self.validate(response: response) {
-                    result.error = error
-                } else {
-                    //TODO: use response encoding, if present
-                    let encoding: String.Encoding = .utf8
-                    
-                    do {
-                        result.value = try P.parse(responseData: data, encoding: encoding)
-                    } catch {
-                        result.error = error
-                    }
-                }
-            }
-            
-            completion(result)
-        }
-        task.resume()
-        
-        return task
     }
     
     public func request<E: Endpoint, R: RequestEncoder>(for endpoint: E, with data: R?=nil) -> URLRequest where E.RequestType == R {
@@ -93,5 +40,67 @@ open class API {
         }
         
         return request
+    }
+    
+    open func validate(response: HTTPURLResponse) -> Error? {
+        let code = response.statusCode
+        if !(200..<300).contains(code) {
+            return APIError.unacceptableStatus(code: code)
+        }
+        
+        return nil
+    }
+    
+    public func complete<P: ParsableResponse>(response: URLResponse?, for request: URLRequest, with data: Data?, error: Error?, debug: Bool, completion: (Result<P>)->()) {
+        var result = Result<P>(response: response as? HTTPURLResponse)
+        
+        if debug || debugAll {
+            if let data = data, let string = String(data: data, encoding: .utf8) {
+                debugPrint("response string for \(request) with status: \(result.response?.statusCode):\n\(string)")
+            } else {
+                debugPrint("no response string for \(request). error: \(result.error). status: \(result.response?.statusCode)")
+            }
+        }
+        
+        if let error = error {
+            result.error = error
+        } else if let response = result.response {
+            if let error = self.validate(response: response) {
+                result.error = error
+            } else {
+                //TODO: use response encoding, if present
+                let encoding: String.Encoding = .utf8
+                
+                do {
+                    result.value = try P.parse(responseData: data, encoding: encoding)
+                } catch {
+                    result.error = error
+                }
+            }
+        }
+        
+        completion(result)
+    }
+    
+    @discardableResult
+    public func call<E: Endpoint, R: RequestEncoder, P: ParsableResponse>(endpoint: E, with data: R?=nil, session: URLSession=URLSession.shared, debug: Bool=false, completion: @escaping (Result<P>)->()) -> URLSessionDataTask where E.RequestType == R, E.ResponseType == P {
+        let request = self.request(for: endpoint, with: data)
+        
+        return start(request: request, for: endpoint, session: session, debug: debug, completion: completion)
+    }
+    
+    @discardableResult
+    public func start<E: Endpoint, P: ParsableResponse>(request: URLRequest, for endpoint: E, session: URLSession=URLSession.shared, debug: Bool=false, completion: @escaping (Result<P>)->()) -> URLSessionDataTask where E.ResponseType == P {
+        return start(request: request, responseType: P.self, session: session, debug: debug, completion: completion)
+    }
+    
+    @discardableResult
+    public func start<P: ParsableResponse>(request: URLRequest, responseType: P.Type, session: URLSession=URLSession.shared, debug: Bool=false, completion: @escaping (Result<P>)->()) -> URLSessionDataTask {
+        let task = session.dataTask(with: request) { data, response, error in
+            self.complete(response: response, for: request, with: data, error: error, debug: debug, completion: completion)
+        }
+        task.resume()
+        
+        return task
     }
 }
