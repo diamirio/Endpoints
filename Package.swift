b1d82e78289986f13b31d0a864d8c99da61b6d6c
Alexander Kauer <contact@kaulex.dev>
2025-10-09 07:33:32 +0200

    Removed SwiftFormat from Package dependencies

--- Package.swift
+++ Package.swift
@@ -17,9 +17,7 @@ let package = Package(
             targets: ["Endpoints"]
         )
     ],
-    dependencies: [
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.50.4")
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "Endpoints",
